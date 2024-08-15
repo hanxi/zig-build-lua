c95580b6f6af83287356c8842f227741a94e557a
涵曦 <im.hanxi@gmail.com>
2024-08-15 09:07:05 +0000

    feat: upgrade zig to 0.13.0

--- build.zig
+++ build.zig
@@ -2,37 +2,37 @@ const std = @import("std");
 
 const Build = std.Build;
 
-pub fn build(b: *Build) void {
+pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
-    const os_tag = target.os_tag orelse
-        (std.zig.system.NativeTargetInfo.detect(target) catch unreachable).target.os.tag;
-
     const lua = b.addExecutable(.{
         .name = "lua",
         .target = target,
     });
 
     lua.linkLibC();
-    //lua.addIncludeDir("lua-5.4.6/src");
 
     const flags = [_][]const u8{
         // Standard version used in Lua Makefile
         "-std=gnu99",
-
-        // Define target-specific macro
-        switch (os_tag) {
-            .linux => "-DLUA_USE_LINUX",
-            .macos => "-DLUA_USE_MACOSX",
-            .windows => "-DLUA_USE_WINDOWS",
-            else => "-DLUA_USE_POSIX",
-        },
     };
 
-    for (lua_54_source_files) |file| {
-        lua.addCSourceFile(.{ .file = .{ .path = b.pathJoin(&.{ "lua-5.4.6/src", file }) }, .flags = &flags });
+    const lua_src_path = "lua-5.4.6/src";
+    var dir = try std.fs.cwd().openDir(lua_src_path, .{ .iterate = true });
+    defer dir.close();
+
+    var dir_it = dir.iterate();
+    while (try dir_it.next()) |entry| {
+        if (entry.kind != .file) {
+            continue;
+        }
+        const file_name = entry.name;
+        if (std.mem.eql(u8, file_name, "luac.c")) {
+            continue;
+        }
+        if (std.mem.endsWith(u8, file_name, ".c")) {
+            lua.addCSourceFile(.{ .file = .{ .cwd_relative = b.pathJoin(&.{ lua_src_path, file_name }) }, .flags = &flags });
+        }
     }
 
     b.installArtifact(lua);
 }
-
-const lua_54_source_files = [_][]const u8{ "lapi.c", "lcode.c", "lctype.c", "ldebug.c", "ldo.c", "ldump.c", "lfunc.c", "lgc.c", "llex.c", "lmem.c", "lobject.c", "lopcodes.c", "lparser.c", "lstate.c", "lstring.c", "ltable.c", "ltm.c", "lundump.c", "lvm.c", "lzio.c", "lauxlib.c", "lbaselib.c", "lcorolib.c", "ldblib.c", "liolib.c", "lmathlib.c", "loadlib.c", "loslib.c", "lstrlib.c", "ltablib.c", "lutf8lib.c", "linit.c", "lua.c" };
