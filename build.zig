d4feb33fbabe38f3209a0d28fd4a8947e57b5ee5
Mark24 <mark.zhangyoung@gmail.com>
2025-02-26 17:30:47 +0800

    update: add macos x86_64

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .x86_64, .os_tag = .windows },
     .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
     .{ .cpu_arch = .aarch64, .os_tag = .linux },
     .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
     .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
